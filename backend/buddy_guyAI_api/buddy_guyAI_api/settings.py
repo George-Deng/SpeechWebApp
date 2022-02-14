2c32904638de2e0c0df5a266c3f78a67120aa136
tliaqat <71024824+tliaqat@users.noreply.github.com>
2022-02-13 23:44:13 -0800

    Update settings.py

--- backend/buddy_guyAI_api/buddy_guyAI_api/settings.py
+++ backend/buddy_guyAI_api/buddy_guyAI_api/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!5g!&&_!$m^uqdp92y(z_73#869a8pw%zmdmb8h=!h*95%(8f7'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
